4ab6292cf2f823e427d56edd23c24122fb5e4c7e
Sumedh <56512410+sumedh27@users.noreply.github.com>
2026-01-05 06:14:17 +0000

    Add useReducer for Custom Hook

--- src/__tests__/hooks/useFetchGetReq.test.jsx
+++ src/__tests__/hooks/useFetchGetReq.test.jsx
@@ -1,49 +1,56 @@
 import { renderHook, waitFor } from "@testing-library/react";
 import useFetchGetReq from "../../hooks/useFetchGetReq";
-import { beforeEach, describe, expect, test, vi } from "vitest";
+import { afterAll, beforeAll, describe, expect, it, vi } from "vitest";
 
-describe("useFetchGetReq", () => {
-  beforeEach(() => {
-    vi.clearAllMocks();
+describe("init", () => {
+  it("should return the initial values for data, error and loading", async () => {
+    const { result } = renderHook(() => useFetchGetReq());
+    const { isLoading, error, products } = result.current;
+
+    expect(isLoading).toBe(true);
+    expect(error).toBe(null);
+    expect(products).toBe(null);
+  });
+});
+
+describe("success", () => {
+  const fetchSpy = vi.spyOn(globalThis, "fetch");
+
+  beforeAll(() => {
+    const mockResolveValue = {
+      ok: true,
+      json: () =>
+        new Promise((resolve) =>
+          resolve([
+            {
+              id: 1,
+              title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+              price: 109.95,
+              description:
+                "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+              category: "men's clothing",
+              image:
+                "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+              rating: {
+                rate: 3.9,
+                count: 120,
+              },
+            },
+          ])
+        ),
+    };
+    fetchSpy.mockReturnValue(mockResolveValue);
   });
 
-  test("initial with loading", () => {
-    globalThis.fetch = vi.fn(() =>
-      Promise.resolve({ ok: true, json: () => Promise.resolve([]) })
-    );
-
-    const { result } = renderHook(() => useFetchGetReq());
-
-    expect(result.current.data).toBe(null);
-    expect(result.current.error).toBe(null);
-    expect(result.current.loading).toBe(true);
+  afterAll(() => {
+    fetchSpy.mockRestore();
   });
 
-  test("return data correctly", async () => {
-    const mockData = { id: 1, title: "test 1" };
-    globalThis.fetch = vi.fn(() =>
-      Promise.resolve({ ok: true, json: () => Promise.resolve(mockData) })
-    );
-
+  it("should fetch products", async () => {
     const { result } = renderHook(() => useFetchGetReq());
 
-    await waitFor(() => {
-      expect(result.current.data).toBe(mockData);
-    });
-
-    expect(result.current.error).toBe(null);
-    expect(result.current.loading).toBe(false);
-  });
-
-  test("return error", async () => {
-    globalThis.fetch = vi.fn(() => Promise.reject(new Error("error")));
-
-    const { result } = renderHook(() => useFetchGetReq());
-
-    await waitFor(() => {
-      expect(result.current.data).toBe(null);
-      expect(result.current.error).toBe("error");
-      expect(result.current.loading).toBe(false);
-    });
+    expect(result.current.isLoading).toEqual(true);
+    await waitFor(() => expect(result.current.products.length).toEqual(1));
+    expect(result.current.isLoading).toEqual(false);
   });
 });
--- src/hooks/useFetchGetReq.jsx
+++ src/hooks/useFetchGetReq.jsx
@@ -1,9 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
+
+function productsReducer(state, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case "error": {
+      return { ...state, isLoading: false, error: payload.err };
+    }
+    case "success": {
+      return { ...state, isLoading: false, products: payload.data };
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${type}`);
+    }
+  }
+}
 
 function useFetchGetReq() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [state, dispatch] = useReducer(productsReducer, {
+    isLoading: true,
+    products: null,
+    error: null,
+  });
 
   useEffect(() => {
     const controller = new AbortController();
@@ -16,20 +33,17 @@ function useFetchGetReq() {
           },
           signal: controller.signal,
         });
+
         if (!res.ok) {
           throw new Error(`HTTP error: Status ${res.status}`);
         }
+
         let data = await res.json();
-        setData(data);
-        setError(null);
+
+        dispatch({ type: "success", payload: { data } });
       } catch (err) {
         if (err.name === "AbortError") return;
-        setError(err.message);
-        setData(null);
-      } finally {
-        setTimeout(() => {
-          setLoading(false);
-        }, 1000);
+        dispatch({ type: "error", payload: { err } });
       }
     };
 
@@ -40,7 +54,7 @@ function useFetchGetReq() {
     };
   }, []);
 
-  return { data, error, loading };
+  return state;
 }
 
 export default useFetchGetReq;
