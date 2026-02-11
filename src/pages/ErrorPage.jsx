f34bcb90f64219118d1a1054ae0ce21bb76dce6e
Sumedh <56512410+sumedh27@users.noreply.github.com>
2026-02-11 08:17:33 +0000

    Change RouterProvider to HashRouter

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,16 +1,10 @@
-import { useRouteError } from "react-router";
 
 export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
-
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+      <h1>404</h1>
+      <p>Page not found</p>
+      <p>The page you are looking for does not exist.</p>
     </div>
   );
 }
